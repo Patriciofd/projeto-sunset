fee4b220304ec923b687416ee714f51a97944692
Patriciofd <Pudimowned@gmail.com>
2015-10-20 17:38:50 -0200

    Desenvolvimento da tarefa 2, Sub-itens 2.1 e 2.2.
    Início e desenvolvimento de dois sub-itens da tarefa 2.

--- PatricioSunset/cadastroUsuario.php
+++ PatricioSunset/cadastroUsuario.php
@@ -1,4 +1,62 @@
 <?php
 include ("conecta.php");
 include ("rotina.php");
+
+$cpf = $_POST["cpf_usuario"];
+$dat = $_POST["nascimento_usuario"]
+
+function ValidaData($dat){
+	$data = explode("/","$dat");
+	$d = $data[0];
+	$m = $data[1];
+	$y = $data[2];
+
+	$res = checkdate($m,$d,$y);
+	if ($res == 1){
+	   return true;
+	} else {
+	   return true;
+	}
+}
+
+	function validaCPF($cpf) {
+    	$cpf = ereg_replace('[^0-9]', '', $cpf);
+    	$cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
+
+    	if (strlen($cpf) != 11) {
+        	return false;
+    	}
+
+   	else if ($cpf == '00000000000' || 
+        	$cpf == '11111111111' || 
+        	$cpf == '22222222222' || 
+        	$cpf == '33333333333' || 
+        	$cpf == '44444444444' || 
+        	$cpf == '55555555555' || 
+        	$cpf == '66666666666' || 
+        	$cpf == '77777777777' || 
+        	$cpf == '88888888888' || 
+        	$cpf == '99999999999') {
+        	return false;
+
+     	} else {   
+         
+        	for ($t = 9; $t < 11; $t++) {
+             
+            		for ($d = 0, $c = 0; $c < $t; $c++) {
+                		$d += $cpf{$c} * (($t + 1) - $c);
+          	  }
+            		$d = ((10 * $d) % 11) % 10;
+            		if ($cpf{$c} != $d) {
+                	return false;
+            }
+        }
+	echo "O seu CPF foi validado com sucesso";
+        return true;
+   		 }	
+	}
+}
+
+
+
 ?>
--- PatricioSunset/conecta.php
+++ PatricioSunset/conecta.php
@@ -2,7 +2,7 @@
 $host = "localhost"; //IP do Host (ou nome)
 $username = "root"; //Usuario do Banco de Dados
 $password = "root"; // Senha do Banco de Dados
-$database = "almoxarifado"; // Database a ser usado
+$database = "sunset"; // Database a ser usado
 $connect = mysql_connect($host, $username, $password) or die (mysql_error());
 mysql_select_db($database, $connect);
 ?>

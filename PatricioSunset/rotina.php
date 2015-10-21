3e6578abffe8537f8a09ba028ad2d84e589b322f
Patriciofd <Pudimowned@gmail.com>
2015-10-21 18:06:44 -0200

    Revert "Inserção ao banco de dados e interface do usuário completa"
    This reverts commit ceb6bac495e0a6c0b181167bb74c6d2be0f85d4f.

--- PatricioSunset/rotina.php
+++ PatricioSunset/rotina.php
@@ -1,7 +1,6 @@
 <?php
-	
 	/*-----------------------------------
-	| 			Cadastrar clientes         |
+	| 			Cadastro de clientes       |
 	-----------------------------------*/
 	function CadastrarClientes($nome, $endereco, $numero, $bairro, $cidade, $estado, $pais, $telefone, $celular, $cpf, $rg_rne, $referencia_comercial_1, $referencia_comercial_2, $referencia_comercial_3) {
 		$sql = "INSERT INTO CLIENTES (	
@@ -29,7 +28,7 @@
 	}
 	
 	/*-----------------------------------
-	| 			Editar clientes            |
+	| 			Editar de clientes         |
 	-----------------------------------*/
 	function EditarClientes($codigo, $nome, $endereco, $numero, $bairro, $cidade, $estado, $pais, $telefone, $celular, $cpf, $rg_rne, $referencia_comercial_1, $referencia_comercial_2, $referencia_comercial_3) {
 		$sql = "UPDATE CLIENTES SET
@@ -154,124 +153,5 @@
 			}		
 	}
 	
-	/*-----------------------------------
-	| 			Cadastrar Usuários         |
-	-----------------------------------*/
-	function CadastrarUsuario($NOME, $SENHA, $EMAIL, $ENDERECO, $CIDADE, $CEP, $ESTADO, $TELEFONE, $CELULAR, $CPF, $NASCIMENTO) {
-		$sql = "INSERT INTO USUARIOS (	
-		NOME,
-		SENHA,
-		EMAIL,
-		ENDERECO,
-		NUMERO,
-		CIDADE,
-		CEP,
-		ESTADO,
-		TELEFONE,
-		CELULAR,
-		CPF,
-		DATA_NASCIMENTO
-		) VALUES ('$NOME','$SENHA', '$EMAIL', '$ENDERECO', '$CIDADE', '$CEP', '$ESTADO','$TELEFONE', '$CELULAR', '$CPF', $NASCIMENTO)";
-		mysql_query($sql) or die (mysql_error());
-		if(mysql_affected_rows() > 0) {
-			echo "Usuário Cadastrado com Sucesso";	
-		} else {
-			echo "Erro ao Cadastrar Usuário";	
-		}
-	}
 	
-	/*-----------------------------------
-	| 			Editar Usuários             |
-	-----------------------------------*/
-	function EditarUsuario($codigo, $NOME, $SENHA, $EMAIL, $ENDERECO, $CIDADE, $CEP, $ESTADO, $TELEFONE, $CELULAR, $CPF, $NASCIMENTO) {
-		$sql = "UPDATE USUARIOS SET 	
-		NOME = '$NOME',
-		SENHA = '$SENHA',
-		EMAIL = '$EMAIL',
-		ENDERECO = '$EMAIL',
-		NUMERO = '$NUMERO',
-		CIDADE = '$CIDADE',
-		CEP = '$CEP',
-		ESTADO = '$ESTADO',
-		TELEFONE = '$TELEFONE',
-		CELULAR = '$CELULAR',
-		CPF = '$CPF',
-		DATA_NASCIMENTO = $NASCIMENTO WHERE CODIGO = $codigo;
-		mysql_query($sql);
-			if(mysql_affected_rows() > 0) {
-				echo "Usuario Alterado com Sucesso";	
-			} else {
-				echo "Erro ao Alterar Usuário";	
-			}		
-	}
-	
-	/*-----------------------------------
-	| 			Excluir Usuários           |
-	-----------------------------------*/
-	function ExcluirUsuario($codigo) {
-		$sql = "DELETE FROM USUARIOS WHERE CODIGO = $codigo";
-		mysql_query($sql);
-			if(mysql_affected_rows() > 0) {
-				echo "Usuário alterado com Sucesso";	
-			} else {
-				echo "Erro ao Alterar Usuário";	
-			}		
-	}
-	
-	/*-----------------------------------
-	| 			Validar Data               |
-	-----------------------------------*/
-	function validaData($dat){
-	$data = explode("/","$dat");
-	$d = $data[0];
-	$m = $data[1];
-	$y = $data[2];
-
-	$res = checkdate($m,$d,$y);
-	if ($res == 1){
-	   return true;
-	} else {
-	   return true;
-	}
-}
-
-	/*-----------------------------------
-	| 			Validar CPF                 |
-	-----------------------------------*/
-	function validaCPF($cpf) {
-    	$cpf = ereg_replace('[^0-9]', '', $cpf);
-    	$cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
-
-    	if (strlen($cpf) != 11) {
-        	return false;
-    	}
-
-   	else if ($cpf == '00000000000' || 
-        	$cpf == '11111111111' || 
-        	$cpf == '22222222222' || 
-        	$cpf == '33333333333' || 
-        	$cpf == '44444444444' || 
-        	$cpf == '55555555555' || 
-        	$cpf == '66666666666' || 
-        	$cpf == '77777777777' || 
-        	$cpf == '88888888888' || 
-        	$cpf == '99999999999') {
-        	return false;
-
-     	} else {   
-         
-        	for ($t = 9; $t < 11; $t++) {
-             
-            		for ($d = 0, $c = 0; $c < $t; $c++) {
-                		$d += $cpf{$c} * (($t + 1) - $c);
-          	  }
-            		$d = ((10 * $d) % 11) % 10;
-            		if ($cpf{$c} != $d) {
-                	return false;
-            }
-        }
-        return true;
-   		 }	
-	}
-}
 ?>

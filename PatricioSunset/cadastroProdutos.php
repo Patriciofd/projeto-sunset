b7c02de006df817ee9b43c4ebc30e51cbb021cf4
Patriciofd <Pudimowned@gmail.com>
2015-11-03 18:25:13 -0200

    Conclusão das Tarefas 4 e 5
    Conclusão das Tarefas 4 e 5 no prazo

---
+++ PatricioSunset/cadastroProdutos.php
@@ -0,0 +1,17 @@
+<?php
+   include ("conecta.php");
+	include ("rotina.php");
+
+	$NOME = $_POST["nome"];
+	$MARCA = $_POST["marca"];
+	$UNIDADES = $_POST["unidades"];
+	$PRECO = $_POST["preco"];
+	$TIPO = $_POST["tipo"];
+	$QUANTIDADE = $_POST["quantidade"];
+
+	if(($NOME == NULL) || ($MARCA == NULL) || (UNIDADES == NULL) || ($PRECO == NULL) || ($TIPO == NULL) || ($QUANTIDADE == NULL){
+			echo "Um dos campos está vazio, preencha todos os campos";	
+	}else {
+					CadastrarProdutos($NOME, $MARCA, $UNIDADES, $PRECO, $TIPO, $QUANTIDADE);
+	}
+?>
--- PatricioSunset/cadastroUsuario.php
+++ PatricioSunset/cadastroUsuario.php
@@ -1,4 +1,30 @@
 <?php
-include ("conecta.php");
-include ("rotina.php");
+	include ("conecta.php");
+	include ("rotina.php");
+
+	$NOME = $_POST["nome_usuario"];
+	$SENHA = $_POST["senha_usuario"];
+	$EMAIL = $_POST["email_usuario"];
+	$ENDERECO = $_POST["endereco_usuario"];
+	$NUMERO = $_POST["numero_usuario"];
+	$CIDADE = $_POST["cidade_usuario"];
+	$CEP = $_POST["cep_usuario"];
+	$ESTADO = $_POST["estado_usuario"];
+	$TELEFONE = $_POST["telefone_usuario"];
+	$CELULAR = $_POST["celular_usuario"];
+	$CPF = $_POST["cpf_usuario"];
+	$NASCIMENTO = $_POST["nascimento_usuario"];
+
+	if(($NOME == NULL) || ($SENHA == NULL) || ($EMAIL == NULL) || ($ENDERECO == NULL) || ($NUMERO == NULL) || ($CIDADE == NULL) || ($EMAIL == NULL) ||  ($ESTADO == NULL) || ($TELEFONE == NULL) ||  ($CELULAR == NULL)){
+		echo "Um dos campos está vazio, preencha todos os campos";	
+	}else {
+		if (validaCPF($CPF)){
+			if(validaData($NASCIMENTO)){
+				if(validaCEP($CEP)){
+					CadastrarUsuario($NOME, $SENHA, $EMAIL, $ENDERECO, $NUMERO, $CIDADE, $CEP, $ESTADO, $TELEFONE, $CELULAR, $CPF, $NASCIMENTO);
+				} 
+			} 	
+		}	
+	}
+
 ?>
